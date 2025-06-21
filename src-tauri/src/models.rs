8bd07f92add653dea21d3dd5e4081138027d5bea
Salim Ali <u14262208@tuks.co.za>
2025-06-21 12:09:58 +0200

    added the rest of the specs

--- src-tauri/src/models.rs
+++ src-tauri/src/models.rs
@@ -7,6 +7,20 @@ pub struct Specs {
 	pub configuration: Configuration,
 	pub response: Response,
 	pub approximation: Approximation,
+	pub passband_ripple: Option<f64>,
+	pub stopband_attenuation: Option<f64>,
+	pub stopband_ripple: Option<f64>,
+	pub passband_attenuation: Option<f64>,
+	pub cutoff_frequency: Option<f64>,
+	pub center_frequency: Option<f64>,
+	pub bandwidth: Option<f64>,
+	pub lower_passband_edge_frequency: Option<f64>,
+	pub upper_passband_edge_frequency: Option<f64>,
+	pub lower_stopband_edge_frequency: Option<f64>,
+	pub upper_stopband_edge_frequency: Option<f64>,
+	pub transition_width: Option<f64>,
+	pub sampling_frequency: Option<f64>,
+	pub order: Option<u16>,
 
 }
 
